109290306504f5ee255790d358632671c29d1838
yyk316507 <1751311442@qq.com>
2018-04-11 06:52:48 +0800

    echarts

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,9 @@
       >{{gameName}}</a>
 
     </div>
+    <div id="myChart" 
+      :style="{width:'300px',height:'300px'}"
+    ></div>
   </div>
 </template>
 
@@ -20,9 +23,30 @@ export default {
     }
     
   },
+  mounted(){
+    this.drawLine();
+  },
   methods:{
     selected:function(gameName){
       this.activeName = gameName
+    },
+    //基于准备好的dom，初始化echarts实例
+    drawLine:function(){
+      let myChart = this.$echarts.init(document.getElementById('myChart'))
+      //绘制图表
+      myChart.setOption({
+        title:{text:'在vue中使用echarts'},
+        tooltip:{},
+        xAxis:{
+          data:["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        },
+        yAxis:{},
+        series:[{
+          name:'销量',
+          type:'bar',
+          data:[5,20,36,10,10,20]
+        }]
+      })
     }
   }
 }
